89f1cfe1c2b3d7bb17ad3cfef7a17cb1c441f61e
hyungjisang <127271150+hyungjisang@users.noreply.github.com>
2023-03-15 10:43:15 +0900

    Add files via upload

---
+++ CastingExam2.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class CastingExam2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = (byte)a;
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DataTypeExam1.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class DataTypeExam1 {
+
+	public static void main(String args[]) {
+		int a = 214748648 ;
+		System.out.println( a );
+		
+
+	}
+
+}
